c1c1dff0e4a0e66e111b25ba394c5dc486c2014e
Eric Romrell <eric_romrell@byu.edu>
2017-12-17 16:25:55 -0700

    Moved questions into separate file

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,12 +1,5 @@
+import json
 import random
-
-QUESTIONS = [
-    {
-        "question": "What male tennis player has won the most major titles in his career?",
-        "answers": ["Roger Federer", "Rafael Nadal", "Pete Sampras", "Rod Laver"],
-        "correct_index": 0
-    }
-]
 
 def lambda_handler(event, context):
     print(event)
@@ -14,7 +7,10 @@ def lambda_handler(event, context):
     if event["session"]["application"]["applicationId"] != "amzn1.ask.skill.bd1dff94-195d-4e65-b69f-234ee4325c4d":
         raise Exception("Invalid Application ID")
 
-    return response(random.choice(QUESTIONS)["question"], False)
+    with open("questions.json") as f:
+        questions = json.load(f)
+
+    return response(random.choice(questions)["question"], False)
 
 def response(output_text, finish_session = False):
     return {
